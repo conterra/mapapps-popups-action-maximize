c17323a7e046f6016c7bb50ed0a75e2d79e1d766
Sebastian Holtkamp <S.Holtkamp@conterra.de>
2025-07-10 17:34:35 +0200

    Update app title in bundle.js to reflect new sample name

--- src/main/js/apps/sample/nls/bundle.js
+++ src/main/js/apps/sample/nls/bundle.js
@@ -15,7 +15,7 @@
  */
 module.exports = {
     root: {
-        apptitle: "DevNet Bundle Blueprint",
+        apptitle: "Popups Action Maximize Sample",
         map: {
             koeln1: {
                 title: "Basic Data",
--- src/main/js/apps/sample/nls/de/bundle.js
+++ src/main/js/apps/sample/nls/de/bundle.js
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 module.exports = {
-    apptitle: "DevNet Bundle Blueprint",
+    apptitle: "Popups Action Maximize Sample",
     map: {
         koeln1: {
             title: "Basisdaten",
